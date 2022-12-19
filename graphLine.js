c7d2dded4a6480f7f1f3b9879cb3bbe2382eb044
Wellinton Rafael Gudilunas <wellintongudilunas788@gmail.com>
2022-12-19 00:36:57 -0300

    Gráfico de Linhas

---
+++ graphLine.js
@@ -0,0 +1,37 @@
+function desenharGraficoLinha() {
+    //Mês gastos 
+    tabela = new google.visualization.DataTable()
+    tabela.addColumn('string', 'Mês');
+    tabela.addColumn('number', 'gastos');
+    tabela.addRows([
+        ['jan', 800],
+        ['fev', 400],
+        ['mar', 1100],
+        ['abr', 400],
+        ['mai', 500],
+        ['jun', 750],
+        ['jul', 1500],
+        ['ago', 650],
+        ['set', 850],
+        ['out', 400],
+        ['nov', 1000],
+        ['dez', 720]
+    ])
+    var opcoes = {
+        title: 'Gráfico de Linhas',
+        legend: 'none', // none = sem legenda
+        width: 950,
+        height: 300,
+        vAxis: {
+                format: 'currency', // no eixo Vertical-Y como moeda
+                gridlines: {count: 3, // linhas de grade fundo
+                            color: 'grey'} // cor das linhas de grade
+                            }, 
+        curveType: 'function'
+
+
+    }
+    var grafico = new google.visualization.LineChart(
+        document.getElementById('graficoLinha'));
+        grafico.draw(tabela, opcoes);
+}
